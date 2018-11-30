caece1af2dda11b52fd7663de0d4f053c47bd9f1
Mohammed Abdelaziz <maa643@pd331.physics.nyu.edu>
2018-11-30 14:55:22 -0500

    .

--- rotatingSpheres.py
+++ rotatingSpheres.py
@@ -1,3 +1,57 @@
 import numpy as np
 
+######################
+### Initialization ###
+######################
 
+N   = 2     # Number of particles
+Nt  = 8000  # Number of timesteps
+dt  = 1e-4  # Duration of one step
+eta = 1e-6  # Viscosity, mks units
+rho = 1e3   # Density of water, mks
+kT  = 4e-21 # Temperature in J
+a   = 1e-6  # All same size, meters 
+
+# Code units
+
+# Initial positions
+
+
+#################
+### Functions ###
+#################
+
+# Correcting for periodic boundaries
+
+# Calculating forces
+
+#################
+### Main Loop ###
+#################
+
+# Calculate velocities
+
+# Update positions
+
+# Deal with particles intersecting? (or just use a smaller timestep?)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#############
+### Plots ###
+#############
