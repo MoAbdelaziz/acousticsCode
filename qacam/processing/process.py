d8b3fee6f2d32ca808f050bdb0a79763f08755fa
Mohammed Abdelaziz <maa643@pd331.physics.nyu.edu>
2019-01-16 12:19:39 -0500

    Updates to plotting code

--- qacam/processing/process.py
+++ qacam/processing/process.py
@@ -3,6 +3,7 @@ import scipy.interpolate as spi
 import os
 import matplotlib.pyplot as plt
 from datetime import date
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 def saveData(data):
 	### Save the current run, automatically dating and numbering the file
@@ -38,21 +39,36 @@ def plotData(data, N):
 	tempX,tempY = np.meshgrid(interpX,interpY)
 	interpCoords = np.vstack([tempX.ravel(),tempY.ravel()]).transpose()
 
-	interpInt = spi.griddata(dataCoords, dataInt, interpCoords, method = 'cubic')
-	interpPhs = spi.griddata(dataCoords, dataPhs, interpCoords, method = 'cubic')
-	print np.nanmin(dataPhs)
-	print np.nanmax(dataPhs)
+	interpInt = spi.griddata(dataCoords, dataInt, interpCoords, method = 'nearest')
+	interpPhs = spi.griddata(dataCoords, dataPhs, interpCoords, method = 'nearest')
+	print np.nanmin(dataY)
+	print np.nanmax(dataY)
+	
+	
 	# Plotting
 	fig, axes = plt.subplots(2,2, sharex='col',sharey='row',figsize=(15,15))
 	axes[0,0].set_title('Intensity Data')
-	axes[0,0].scatter(dataX,dataY, c = dataInt, cmap='inferno') # Intensity Data
-	p1=axes[1,0].scatter(interpCoords[:,0],interpCoords[:,1], c = interpInt, cmap='inferno') #Interpolated
-	#fig.colorbar(p1,axes[1,0])
-
+	p1=axes[0,0].scatter(dataX,dataY, c = dataInt, cmap='inferno') # Intensity Data
+	p2=axes[1,0].scatter(interpCoords[:,0],interpCoords[:,1], c = interpInt, cmap='inferno') #Interpolated
+	
+	divider = make_axes_locatable(axes[0,0]) # Ugly block to make colorbars work
+	cax = divider.append_axes('right',size='5%',pad=.05)
+	fig.colorbar(p1,cax=cax,orientation='vertical')
+	divider = make_axes_locatable(axes[1,0])
+	cax = divider.append_axes('right',size='5%',pad=.05)
+	fig.colorbar(p2,cax=cax,orientation='vertical')
+	
 	axes[0,1].set_title('Phase Data')
-	axes[0,1].scatter(dataX,dataY, c = dataPhs, cmap='hsv') # Phase Data
+	p1=axes[0,1].scatter(dataX,dataY, c = dataPhs, cmap='hsv') # Phase Data
 	p2=axes[1,1].scatter(interpCoords[:,0],interpCoords[:,1], c = interpPhs, cmap='hsv') #Interpolated
-	#fig.colorbar(p2,axes[1,1])
+	
+	divider = make_axes_locatable(axes[0,1])
+	cax = divider.append_axes('right',size='5%',pad=.05)
+	fig.colorbar(p1,cax=cax,orientation='vertical')
+	divider = make_axes_locatable(axes[1,1])
+	cax = divider.append_axes('right',size='5%',pad=.05)
+	fig.colorbar(p2,cax=cax,orientation='vertical')
+	
 	axes[0,0].invert_yaxis() # Invert y-axes since acam uses positive y-axis downward
 	axes[1,0].invert_yaxis()
 	plt.show()
---
+++ qacam/processing/process.py~
@@ -0,0 +1,64 @@
+import numpy as np
+import scipy.interpolate as spi
+import os
+import matplotlib.pyplot as plt
+from datetime import date
+
+def saveData(data):
+	### Save the current run, automatically dating and numbering the file
+	i = 1
+	today = str(date.today())
+	filename =  today+"_N%s.npy"
+	
+	while os.path.exists(filename % i):
+		i +=1
+	
+	np.save(filename % i, data)
+	return
+
+def plotData(data, N):
+	### Plot intensity and phase data on interpolated grid 
+	#N: interpolation points in each direction 
+	#data:assumed 2D data with real/imaginary (columns are: x,y,real,imag)
+	
+	# Interpolation
+	dataX = testData[:,0]
+	dataY = testData[:,1]
+	dataCoords = np.stack((dataX,dataY),axis=1)
+	dataReal   = testData[:,2]
+	dataImag   = testData[:,3]
+	
+	dataInt    = dataReal**2 + dataImag**2
+	dataPhs    = np.arctan2(dataImag,dataReal)
+	
+	interpX = np.linspace(np.amin(dataX), np.amax(dataX), num=N)
+	interpY = np.linspace(np.amin(dataY), np.amax(dataY), num=N)
+	
+	tempX,tempY = np.meshgrid(interpX,interpY)
+	interpCoords = np.vstack([tempX.ravel(),tempY.ravel()]).transpose()
+
+	interpInt = spi.griddata(dataCoords, dataInt, interpCoords, method = 'cubic')
+	interpPhs = spi.griddata(dataCoords, dataPhs, interpCoords, method = 'cubic')
+	
+	# Plotting
+	fig, axes = plt.subplots(2,2, sharex='col',sharey='row')
+	axes[0,0].set_title('Intensity Data')
+	axes[0,0].scatter(dataX,dataY, c = dataInt) # Intensity Data
+	axes[1,0].scatter(interpCoords[:,0],interpCoords[:,1], c = interpInt) #Interpolated
+	
+	axes[0,1].set_title('Phase Data')
+	axes[0,1].scatter(dataX,dataY, c = dataPhs) # Phase Data
+	axes[1,1].scatter(interpCoords[:,0],interpCoords[:,1], c = interpPhs) #Interpolated
+	plt.show()
+	return
+	
+def testData():
+	### Generate test data
+	dataCoords = np.random.rand(10000,2)
+	dataVals1   = 10*np.random.rand(10000,1)
+	dataVals2   = -10*np.random.rand(10000,1)
+	testData = np.concatenate((dataCoords,dataVals1,dataVals2),axis=1)
+	return testData
+	
+testData = testData()
+plotData(testData,50)
