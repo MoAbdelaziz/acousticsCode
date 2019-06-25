2d7c1a1371d90a6276409ff21862371146900a09
Mohammed Abdelaziz <maa643@nyu.edu>
2019-06-25 15:47:23 -0400

    New image files/code for harmonic vortex figure

--- acoustokinetic/sphereFigure.py
+++ acoustokinetic/sphereFigure.py
@@ -6,11 +6,12 @@ import matplotlib.patches as pat
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import rc
+from mayavi import mlab
 
 ## Parameters
 
-xRange = 2  
-yRange = 2
+xRange = 10
+yRange = 10
 num    = 1000     # Number of points in each direction
 
 
@@ -31,7 +32,7 @@ def thetaC(x,y):
   
   
 def field(r):
-    return r**2
+    return .5*r**2*np.exp(-r/3)
 def phase(theta):
     return theta % (2*np.pi)
 
@@ -47,11 +48,11 @@ ax.plot_surface(xG,yG, field(rC(xG,yG)) , rstride=100 ,cstride=100, cmap=None,al
 ## Draw the sphere
 u, v = np.mgrid[0:np.pi:50j, 0:2*np.pi:50j]
 
+R=1.1
 
-x0=.25
-y0=.25
-z0=x0**2+y0**2+0.1
-R=.25
+x0=-R
+y0=R
+z0=x0**2+y0**2+R/2
 
 x = R * np.sin(u) * np.cos(v)+x0
 y = R * np.sin(u) * np.sin(v)+y0
@@ -78,4 +79,27 @@ ax.set_zlim3d( [0,         field(rC(xRange/2,yRange/2))])
 ax.view_init(45,160)
 fig.tight_layout()
 
-plt.show()
+#plt.show()
+
+
+## Using Mayavi2/mlab instead of matplotlib/mplot3d ##
+res=100 # sparseness of wireframe relative to full surface data. Higher number, more sparse
+trap = mlab.mesh(xG,yG, field (rC (xG,yG ) ) ,scalars =  phase(thetaC(xG,yG))/(2*np.pi), colormap = 'hsv')
+trapBacking = mlab.mesh(xG,yG, field (rC (xG,yG ) )-0.01 ,color=(0,0,0) )
+#trapFrame = mlab.mesh(xG[0::res,0::res],yG[0::res,0::res], field (rC (xG,yG )[0::res,0::res] ) ,color = (0,0,0) , representation = 'wireframe', mode='axes',opacity=.1)
+#trapFrame = mlab.surf(xG[0::res,0::res],yG[0::res,0::res], field (rC (xG,yG )[0::res,0::res] ))
+drawNum = int(num/res)
+for i in range(0,drawNum): #Just draw num/res lines for a custom square gridding...
+	xPoints1 = np.ones(drawNum)*i*(xRange/drawNum)-xRange/2
+	yPoints1 = np.linspace(-yRange/2,yRange/2,drawNum)
+	zPoints1 = field(rC(xPoints1,yPoints1))
+	mlab.plot3d(xPoints1 , yPoints1  , zPoints1,color=(0,0,0),opacity=0.1)
+	
+	xPoints2 = np.linspace(-xRange/2,xRange/2,drawNum)
+	yPoints2 = np.ones(drawNum)*i*(yRange/drawNum)-yRange/2
+	zPoints2 = field(rC(xPoints2,yPoints2))
+	mlab.plot3d(xPoints2 , yPoints2  , zPoints2,color=(0,0,0),opacity=0.1)
+sphere = mlab.mesh(x,y,z, color=(1,1,1) )
+#mlab.axes(extent=[-xRange/2,xRange/2,-yRange/2,yRange/2,0,field(rC(xRange/2,yRange/2))])
+mlab.savefig(filename = 'harmonicVortex.png')
+mlab.show()
